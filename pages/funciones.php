a92b181c89b0919a8213e3ed8694e828f1229fce
alex1 <alex14lf@gmail.com>
2023-11-24 16:23:21 +0100

    creacion funcion conectarBd

--- pages/funciones.php
+++ pages/funciones.php
@@ -1,4 +1,11 @@
 <?php
+function ConectarBd() {
+    $cadena_conexion = 'mysql:dbname=clubdeportivo;host=127.0.0.1';
+    $usuario = 'root';
+    $clave = '';
+    $bd = new PDO($cadena_conexion, $usuario, $clave);
+    return $bd;
+}
 
 function comprobar_usuario($nombre, $clave, $usuarios) {
 
